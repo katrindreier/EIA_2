fa7e8c4fdf643db4b313fcdc019c1b66a131a309
katrindreier <katrindreier@katrins-air>
2018-03-24 10:12:38 +0100

    Aufgabe00

--- Aufgabe00/Aufgabe00.js
+++ Aufgabe00/Aufgabe00.js
@@ -1,3 +1,13 @@
+/*
+Aufgabe 00
+Name: Katrin Dreier
+Matrikel: 257680
+Datum: 23.03.18
+
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
 function greet() {
     var name = prompt("Wer bist du?", "");
     if (name != null) {
