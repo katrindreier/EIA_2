db2d50191a349cf844f8b6f29a2bd5673beb911d
katrindreier <katrindreier@katrins-air>
2018-03-25 11:04:56 +0200

    Aufgabe00

--- Aufgabe00/Aufgabe00.js
+++ Aufgabe00/Aufgabe00.js
@@ -3,15 +3,14 @@ Aufgabe 00
 Name: Katrin Dreier
 Matrikel: 257680
 Datum: 23.03.18
-
 Hiermit versichere ich, dass ich diesen
 Code selbst geschrieben habe. Er wurde
 nicht kopiert und auch nicht diktiert.
 */
-function greet() {
+function hello() {
     var name = prompt("Wer bist du?", "");
     if (name != null) {
-        document.getElementById("inputname").innerHTML =
+        document.getElementById("greet").innerHTML =
             "Ahh, hallo, du bist es " + name + " :) ";
     }
 }
--- Aufgabe00/Aufgabe00.ts
+++ Aufgabe00/Aufgabe00.ts
@@ -3,7 +3,6 @@ Aufgabe 00
 Name: Katrin Dreier 
 Matrikel: 257680
 Datum: 23.03.18
-
 Hiermit versichere ich, dass ich diesen
 Code selbst geschrieben habe. Er wurde
 nicht kopiert und auch nicht diktiert. 
@@ -11,13 +10,12 @@ nicht kopiert und auch nicht diktiert.
 
 
     
-function greet() : void {
+function hello() : void {
     var name : string = prompt("Wer bist du?", "");
     if (name != null) {
-        document.getElementById("inputname").innerHTML =
+        document.getElementById("greet").innerHTML =
             "Ahh, hallo, du bist es " + name + " :) ";
     }
 }
-    
-    
+
 
