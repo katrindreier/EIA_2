71f052064585c5ed937e8d2b6f97032b2fff1abe
katrindreier <katrindreier@katrins-air.fu.hfu_guest.hs-furtwangen.de>
2018-04-12 17:14:18 +0200

    Aufgabe 2

---
+++ Aufgabe02/Aufgabe2.js
@@ -0,0 +1,7 @@
+var Aufgabe2;
+(function (Aufgabe2) {
+    let numPlayers;
+    let numPairs;
+    let cardContent = ["A", "B", "C", "D", "E", "F", "G"];
+})(Aufgabe2 || (Aufgabe2 = {}));
+//# sourceMappingURL=Aufgabe2.js.map
---
+++ Aufgabe02/Aufgabe2.ts
@@ -0,0 +1,15 @@
+namespace Aufgabe2 {
+ 
+    let numPlayers: number=0;
+    let numPairs: number; 
+    let cardContent: string[]= ["A","B","C","D","E","F","G"];
+    
+    while (numPlayers <= 5) {
+    numPlayers = prompt("Wie viele spielen mit?");  
+         
+    } 
+
+    
+    
+    
+}
